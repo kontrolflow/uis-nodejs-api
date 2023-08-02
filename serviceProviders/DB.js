e19e7974403ce7ddb2740bc701ac08d688729224
Jullian Abinsay <jullianabinsay@gmail.com>
2023-08-02 09:27:21 -0400

    Adds a Connection to a Mongo DB Server

---
+++ serviceProviders/DB.js
@@ -0,0 +1,32 @@
+const {MongoClient} = require('mongodb');
+const url = "mongodb://"+process.env.MDB_USER+":"+process.env.MDB_PASS+"@"+process.env.MDB_HOST+"/"
+const db = process.env.MDB_DB
+
+class DB {
+
+    client;
+
+    constructor(db) {
+        this.connect()
+    }
+
+    async connect() {
+        this.client = new MongoClient(url);
+        try {
+            await this.client.connect();  
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    query() {
+        return this.client.db(db);
+    }
+
+    async close() {
+        await this.client.close();
+    }
+ 
+}
+
+module.exports = DB
