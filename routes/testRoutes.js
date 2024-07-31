0880e86fad0df87b1bf65a53d57bafe566c40a8d
Jullian Abinsay <jullian@abinsay.com>
2024-07-31 08:35:37 -0400

    Changes version number

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // const DB = require("../serviceProviders/DB")
 
 router.get('/version', async (req, res) => {
-    let version = 2.3
+    let version = 2.4
     console.log("Version: " + version)
     res.send("Version: " + version)
 })
