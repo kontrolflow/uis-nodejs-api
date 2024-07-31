98b260db534fcd10e26bd93ff9ecc4cea3f2be99
Jullian Abinsay <jullian@abinsay.com>
2024-07-31 16:52:03 -0400

    Installs Successful POC for Successful backup webhooks. Adds function for adding time entries to tickets for Tara

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -1,5 +1,6 @@
 // Imports for API Routing
 const express = require('express');
+const AutoTaskAPI = require('../serviceProviders/AutoTaskAPI');
 const router = express.Router();
 
 router.post('/5000-successful-backup-event', async (req, res) => {
@@ -7,6 +8,33 @@ router.post('/5000-successful-backup-event', async (req, res) => {
     console.log("Webhook Route")
     console.log("5000 - Successful Backup Event")
     console.log(req.body)
+    persistPayload(req.body)
+
+    // Prep Data
+    const device_hostname = req.body.device_hostname
+    let foundTicket = false
+
+    // Find All Open Tickets Matching VM Backup Offsite Copy Failed
+    const matches = await AutoTaskAPI.getOpenTicketsMatchingTitle("VM Local Backup Failed")
+    // console.log(matches)
+    
+    // Loop Through Matches Looking for the Hostname in the Description
+    matches.forEach(ticket => {
+        console.log(device_hostname)
+        console.log(ticket)
+        if(ticket.description.includes(device_hostname)) {
+            foundTicket = ticket
+        }
+    });
+
+    // If ticket is found, add billing and mark as complete
+    if(foundTicket) {
+        console.log("match found!")
+        const billingEntryNote = "Confirmed successful backup occurence following failure."
+        AutoTaskAPI.addBillingToTicket(foundTicket.id, billingEntryNote, 30)
+    }
+
+    console.log(matches)
 
     res.status(200).send("Webhook Triggered / 5002 - Successful Backup Event")
 
@@ -17,6 +45,7 @@ router.post('/5003-successful-restore-or-verification-event', async (req, res) =
     console.log("Webhook Route")
     console.log("5003 - Successful Backup Event")
     console.log(req.body)
+    persistPayload(req.body)
 
     res.status(200).send("Webhook Triggered / 5003 - Successful Restore or Verification Event")
 
@@ -27,9 +56,44 @@ router.post('/5005-successful-offsite-copy-event', async (req, res) => {
     console.log("Webhook Route")
     console.log("5005 - Successful Offsite Copy Event")
     console.log(req.body)
+    persistPayload(req.body)
 
     res.status(200).send("Webhook Triggered / 5005 - Successful Offsite Copy Event")
 
 })
+
+async function persistPayload(payload) {
+
+    return new Promise(resolve => {
+
+        const axios = require('axios');
+
+        const url = "https://prod-188.westus.logic.azure.com:443/workflows/280b8a022ed541559e63eac114ab7d5b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2zhKa5ZNtRFKz8lamGyrWBZf95Ch64bYRIdadhQdFPw"
+
+        var options = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+
+
+        axios.post(url, payload, options)
+            .then(res=> {
+                console.log(res.data)
+                if(res.data) {
+                    resolve(true)
+                } else {
+                    resolve(false)
+                }
+            })
+            .catch(err=> {
+                resolve(false)
+                console.log(err)
+                console.log(err.response.data.errors)
+            })
+
+    })
+
+}
 
 module.exports = router
--- serviceProviders/AutoTaskAPI.js
+++ serviceProviders/AutoTaskAPI.js
@@ -145,6 +145,117 @@ class AutoTaskAPI {
 
     }
 
+    static getOpenTicketsMatchingTitle(title) {
+
+        return new Promise(resolve => {
+
+            const axios = require('axios');
+
+            // console.log(id)
+
+            const apiBaseURL = "https://webservices14.autotask.net/atservicesrest/v1.0/"
+
+            // const urlOptions = 'Companies/query?search={ "filter":[{"op" : "exist", "field" : "id" }],"IncludeFields": ["Id", "isactive","companyName", "phone"]}'
+            const urlOptions = 'tickets/query?search={"filter":[{"op":"eq","field":"Title","value":"' + title + '"},{"op":"noteq","field":"Status","value":5}]}'
+
+
+            const url = apiBaseURL + urlOptions
+
+            console.log(url)
+
+            var options = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'ApiIntegrationCode':process.env.AUTOTASK_INTEGRATION_CODE,
+                    'UserName':process.env.AUTOTASK_USER,
+                    'Secret':process.env.AUTOTASK_SECRET
+                }
+            }
+
+            axios.get(url, options)
+            .then(res=> {
+                resolve(res.data.items)
+                // console.log("Items:")
+                // console.log(res.data.items)
+            })
+            .catch(err=> {
+                resolve(false)
+                console.log(err)
+            })
+
+        })
+
+    }
+
+    static async addBillingToTicket(id, note, duration) {
+
+        return new Promise(resolve => {
+
+            const axios = require('axios');
+
+            console.log(id)
+
+            const url = "https://webservices14.autotask.net/atservicesrest/v1.0/TimeEntries"
+
+            var options = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'ApiIntegrationCode':process.env.AUTOTASK_INTEGRATION_CODE,
+                    'UserName':process.env.AUTOTASK_USER,
+                    'Secret':process.env.AUTOTASK_SECRET
+                }
+            }
+
+            // Calculate Timestamps for duration
+
+            const endTimeStamp =  new Date();
+
+            const MS_PER_MINUTE = 60000;
+            const durationInMinutes = duration;
+            const startTimeStamp = new Date(endTimeStamp - durationInMinutes * MS_PER_MINUTE);
+            
+            // console.log(formattedDate); 
+
+            const body = {
+                "TicketID": id,
+                "ResourceID": 29682926,
+                "StartDateTime": startTimeStamp,
+                "EndDateTime": endTimeStamp,
+                "RoleID": 29683461,
+                "SummaryNotes": note
+            }
+
+            // const body = {
+            //     "TicketID": id,
+            //     "ResourceID": 29682926,
+            //     "StartDateTime": "Jun 14 2024 20:02:39",
+            //     "EndDateTime": "Jun 14 2024 20:30:39",
+            //     "RoleID": 29683461,
+            //     "SummaryNotes": note
+            // }
+
+            console.log(body)
+
+
+            axios.post(url, body, options)
+                .then(res=> {
+                    console.log(res.data)
+                    if(res.data.itemId) {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                })
+                .catch(err=> {
+                    resolve(false)
+                    console.log(err)
+                    console.log(err.response.data.errors)
+                })
+
+        })
+
+    }
+
     static test() {
         return true
     }
