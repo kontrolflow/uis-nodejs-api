d1af63a7b9a8f75292ad58f9bd41496688d84ff4
Jullian Abinsay <jullian@abinsay.com>
2024-08-04 15:31:17 -0400

    Adds Backup Status webhooks for codes 5003 and 5005

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -14,7 +14,7 @@ router.post('/5000-successful-backup-event', async (req, res) => {
     const device_hostname = req.body.device_hostname
     let foundTicket = false
 
-    // Find All Open Tickets Matching VM Backup Offsite Copy Failed
+    // Find All Open Tickets Matching VM Local Backup Copy Failed
     const matches = await AutoTaskAPI.getOpenTicketsMatchingTitle("VM Local Backup Failed")
     // console.log(matches)
     
@@ -29,14 +29,24 @@ router.post('/5000-successful-backup-event', async (req, res) => {
 
     // If ticket is found, add billing and mark as complete
     if(foundTicket) {
-        console.log("match found!")
-        const billingEntryNote = "Confirmed successful backup occurence following failure."
-        AutoTaskAPI.addBillingToTicket(foundTicket.id, billingEntryNote, 30)
+        console.log("Matching Ticket Found")
+
+        // Add a 15 Minute Time Entry w/ Note
+        const date =  new Date();
+        const readableTime = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        const readableDate = date.toDateString()
+        const billingEntryNote = "Confirmed a successful local backup occurence proceeding this failure.\r\n\r\n - Backup time was on " + readableDate + " at " + readableTime + "\r\n\r\n"
+        AutoTaskAPI.addBillingToTicket(foundTicket.id, billingEntryNote, 15)
+
+        // Complete the Ticket
+        const fields = { status:5 }
+        AutoTaskAPI.updateTicket(foundTicket.id, fields)
+
+    } else {
+        console.log("No ticket found")
     }
 
-    console.log(matches)
-
-    res.status(200).send("Webhook Triggered / 5002 - Successful Backup Event")
+    res.status(200).send("Webhook Triggered / 5000 - Successful Backup Event")
 
 })
 
@@ -46,6 +56,42 @@ router.post('/5003-successful-restore-or-verification-event', async (req, res) =
     console.log("5003 - Successful Backup Event")
     console.log(req.body)
     persistPayload(req.body)
+
+    // Prep Data
+    const device_hostname = req.body.device_hostname
+    let foundTicket = false
+
+    // Find All Open Tickets Matching VM Backup - Failed Restore or Verification
+    const matches = await AutoTaskAPI.getOpenTicketsMatchingTitle("VM Backup - Failed Restore or Verification")
+    // console.log(matches)
+    
+    // Loop Through Matches Looking for the Hostname in the Description
+    matches.forEach(ticket => {
+        console.log(device_hostname)
+        console.log(ticket)
+        if(ticket.description.includes(device_hostname)) {
+            foundTicket = ticket
+        }
+    });
+
+    // If ticket is found, add billing and mark as complete
+    if(foundTicket) {
+        console.log("Matching Ticket Found")
+
+        // Add a 15 Minute Time Entry w/ Note
+        const date =  new Date();
+        const readableTime = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        const readableDate = date.toDateString()
+        const billingEntryNote = "Confirmed a successful restore/verification occurence proceeding this failure.\r\n\r\n - Restore/verification was on " + readableDate + " at " + readableTime + "\r\n\r\n"
+        AutoTaskAPI.addBillingToTicket(foundTicket.id, billingEntryNote, 30)
+
+        // Complete the Ticket
+        const fields = { status:5 }
+        AutoTaskAPI.updateTicket(foundTicket.id, fields)
+
+    } else {
+        console.log("No ticket found")
+    }
 
     res.status(200).send("Webhook Triggered / 5003 - Successful Restore or Verification Event")
 
@@ -57,6 +103,42 @@ router.post('/5005-successful-offsite-copy-event', async (req, res) => {
     console.log("5005 - Successful Offsite Copy Event")
     console.log(req.body)
     persistPayload(req.body)
+
+    // Prep Data
+    const device_hostname = req.body.device_hostname
+    let foundTicket = false
+
+    // Find All Open Tickets Matching VM Backup Offsite Copy Failed 
+    const matches = await AutoTaskAPI.getOpenTicketsMatchingTitle("VM Backup Offsite Copy Failed")
+    // console.log(matches)
+    
+    // Loop Through Matches Looking for the Hostname in the Description
+    matches.forEach(ticket => {
+        console.log(device_hostname)
+        console.log(ticket)
+        if(ticket.description.includes(device_hostname)) {
+            foundTicket = ticket
+        }
+    });
+
+    // If ticket is found, add billing and mark as complete
+    if(foundTicket) {
+        console.log("Matching Ticket Found")
+
+        // Add a 15 Minute Time Entry w/ Note
+        const date =  new Date();
+        const readableTime = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        const readableDate = date.toDateString()
+        const billingEntryNote = "Confirmed a successful offsite backup occurence proceeding this failure.\r\n\r\n - Offsite backup was on " + readableDate + " at " + readableTime + "\r\n\r\n"
+        AutoTaskAPI.addBillingToTicket(foundTicket.id, billingEntryNote, 30)
+
+        // Complete the Ticket
+        const fields = { status:5 }
+        AutoTaskAPI.updateTicket(foundTicket.id, fields)
+
+    } else {
+        console.log("No ticket found")
+    }
 
     res.status(200).send("Webhook Triggered / 5005 - Successful Offsite Copy Event")
 
