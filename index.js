e0f943de222aa4e04a1dba4751ca0093d32ae99a
Jullian Abinsay <jullian@abinsay.com>
2024-08-14 12:29:40 -0400

    Adds  VCard Download options for QR codes

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ app.get('/contact/:userId', (req, res) => {
          referer : req.query.referer
     }
 
+    if(req.params.userId == "rdonelly") {
+        var file = ('./UmbrellaContactCards/RayDonelly.vcf');
+        res.download(file)
+        return
+    }
+
     if(response.referer == "qr-code") {
         console.log(response)
         res.send(response)
